b789cb917f0645c42ccd5dee537fbbd83b399379
unknown <fragas212@gmail.com>
2021-12-07 02:03:12 +0100

    2048_utils.cpp

---
+++ 2048_utils.cpp
@@ -0,0 +1,11 @@
+#include "2048_stuff.h"
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	system("color 0B"); // Change la couleur du terminal
+	game();
+	return 0;
+}
